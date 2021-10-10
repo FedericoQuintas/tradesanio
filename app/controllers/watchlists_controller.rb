832e65222d5fb2242b4f1b55edec71bb9e2f1503
Federico Quintas <federico.quintas@careem.com>
2021-10-10 16:07:24 +0200

    Userid comes from header

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -2,11 +2,11 @@ class WatchlistsController < ApplicationController
     def create
         if parse_params['description'].blank?
             render plain: {}.to_json, status: 400, content_type: 'application/json'
-        else 
-            if Watchlist.where(:description => parse_params["description"], :user_id => 1).present?
+        else
+            if Watchlist.where(:description => parse_params["description"], :user_id => request.headers['X-User-ID']).present?
                 render plain: {error: "Description repeated"}.to_json, status: 400, content_type: 'application/json' 
             else
-                @watchList = Watchlist.create(description: parse_params["description"], user_id: 1)
+                @watchList = Watchlist.create(description: parse_params["description"], user_id: request.headers['X-User-ID'])
                 render plain: {description: @watchList.description}.to_json, status: 201, content_type: 'application/json'
             end
         end
