74fda8d5d5457bc8a30c53fc5000c7f38123e191
Federico Quintas <federico.quintas@careem.com>
2021-10-16 12:48:41 +0200

    Use let for test

--- spec/requests/create_watch_list_spec.rb
+++ spec/requests/create_watch_list_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "CreateWatchLists", type: :request do
-  it 'post /watchlists' do
-    @description = "crypto"
 
-    post "/watchlists", :params => { :description => @description} 
+  let(:description) {
+      @description = "crypto"
+  }
+  it 'post /watchlists' do
+
+    post "/watchlists", :params => { :description => description} 
     expect(response).to have_http_status(:created)
     expect(response.content_type).to eq("application/json; charset=utf-8")
-    expect(JSON.parse(response.body)['description']).to eq(@description)
+    expect(JSON.parse(response.body)['description']).to eq(description)
   end
 
   it 'fails when watchlist with same description exists' do
@@ -17,13 +20,11 @@ RSpec.describe "CreateWatchLists", type: :request do
 
     post "/users", :params => { :email => @user_email, :pass => @pass} , :headers => {'X-User-ID' => 1}
 
-    @description = "crypto"
-
-    post "/watchlists", :params => { :description => @description}  , :headers => {'X-User-ID' => 1}
+    post "/watchlists", :params => { :description => description}  , :headers => {'X-User-ID' => 1}
     expect(response).to have_http_status(:created)
     expect(response.content_type).to eq("application/json; charset=utf-8")
-    expect(JSON.parse(response.body)['description']).to eq(@description)
-    post "/watchlists", :params => { :description => @description}  , :headers => {'X-User-ID' => 1}
+    expect(JSON.parse(response.body)['description']).to eq(description)
+    post "/watchlists", :params => { :description => description}  , :headers => {'X-User-ID' => 1}
     expect(response).to have_http_status(:bad_request)
     expect(JSON.parse(response.body)['error']).to eq("Description repeated")
     expect(response.content_type).to eq("application/json; charset=utf-8")
