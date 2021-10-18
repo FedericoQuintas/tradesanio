ed02528847943b3c8710b0ce04b4a8771453577b
Federico Quintas <federico.quintas@careem.com>
2021-10-18 19:40:09 +0200

    Refactor test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
+
+    it 'is valid when has the pass and email' do
+        expect(User.create(:email => 'email', :pass => 'pass')).to be_valid
+    end
+
     it 'raises error if pass is missing' do
-        expect(User.create(:email => 'email').valid?).to be(false)
+        expect(User.create(:email => 'email')).to_not be_valid
     end
 
     it 'raises error if email is missing' do
-        expect(User.create(:pass => 'pass').valid?).to be(false)
+        expect(User.create(:pass => 'pass')).to_not be_valid
     end
 end
